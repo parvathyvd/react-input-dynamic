8931b94b3af075b7c6e19dbf28305bd7fdcb9424
parvathyvd <parvathyvd@gmail.com>
2018-06-29 14:58:35 -0700

    added some styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Person from './Person.css'
 
 const person = (props)=> {
     return(
-        <div>
+        <div className="person">
         <p>This text comes from the person functional component</p>
         <input type="text" onChange={props.changed} value={props.name}/>
-        <h3 onClick ={props.click}>my name is {props.name} and age is {props.age} </h3>
+        <h3 onClick ={props.click}>My name is {props.name} and age is {props.age} .If you click on the text(me) I will get deleted</h3>
         </div>
     )
 }
